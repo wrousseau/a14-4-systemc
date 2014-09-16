65bfafff795efe94897995b4d0bb9d4ad584a0fa
wrousseau <woody.rousseau@ensta-paristech.fr>
2014-09-16 10:47:32 +0200

    .gitignore

--- module_a14_tp1/filter/src/filter.cpp
+++ module_a14_tp1/filter/src/filter.cpp
@@ -3,55 +3,60 @@
 //do_filter
 void 
 filter::do_filter()
+{
+  while(1)
   {
-    while(1)
+    wait();
+
+    //Check the address
+    if(addr==0x20000000) 
     {
-      wait();
-
-      //Check the address
-      if(addr==0x20000000) 
+      //Write
+      if(oen && !wen) 
       {
-        //Write
-        if(oen && !wen) {
-          if(!mem->nb_write(data)) {
-	    cout<<"[filter]: internal fifo is too small (size="<<mem->num_free()<<")."<<endl;
-            exit(-1);
-          }
-        }
-
-        //Read
-        else if(!oen && wen) {
-          unsigned char value;
-          if(!mem->nb_read(value)) {
-	    cout<<"[filter]: internal fifo is empty."<<endl; 
-	  }
-          else data = value;
-        }
-        
-        //Main processing
-        else if(data_in_ready.read())
+        if(!mem->nb_write(data)) 
         {
-          //We have received all the data.
-          //Processing can start
-          cout<<"[filter]: start processing."<<endl;
-          wait(100);
-          cout<<"[filter]: done."<<endl;
-          data_out_ready.write(true);
-        }
-
-        //Init
-        else if(!data_in_ready.read())
-        {
-          data_out_ready.write(false);
+         cout<<"[filter]: internal fifo is too small (size="<<mem->num_free()<<")."<<endl;
+         exit(-1);
         }
       }
-      else {
-        //Not concerned
+
+      //Read
+      else if(!oen && wen) 
+      {
+        unsigned char value;
+        if(!mem->nb_read(value)) 
+        {
+          cout<<"[filter]: internal fifo is empty."<<endl; 
+        }
+        else data = value;
+      }
+
+      //Main processing
+      else if(data_in_ready.read())
+      {
+        //We have received all the data.
+        //Processing can start
+        cout<<"[filter]: start processing."<<endl;
+        wait(100);
+        cout<<"[filter]: done."<<endl;
+        data_out_ready.write(true);
+      }
+
+      //Init
+      else if(!data_in_ready.read())
+      {
+        data_out_ready.write(false);
       }
     }
+    else 
+    {
+      //Not concerned
+    }
   }
+}
 
-	
+
 //Constructor
 filter::filter(sc_module_name name):sc_module(name)
 {
